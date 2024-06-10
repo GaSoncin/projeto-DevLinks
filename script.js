cec528c91b908a8f8526e1ab810e41b5936e27cc
GaSoncin <gabrielsoncindev@gmail.com>
2024-06-10 19:43:31 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+  /* if(html.classList.contains('light')) {
+    html.classList.remove('light')
+  } else {
+    html.classList.add('light')
+  } */
+  
+  /* .pegar a tag img!
+  const img = document.querySelector('#profile img')
+  .substituir a imagem
+  if(html.classList.contains('light')) {
+  .light mode, adicionar avatar-light
+  img.setAttribute('src', './assets/avatar-light.png')
+  } else {
+  .dark mode, manter imagem normal
+    img.setAttribute('src', './assets/avatar.png')
+  } */
+}
